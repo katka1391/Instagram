410fca27671bbda4e12b812b3759f96b26c3c181
Katarina Slivova <katka.slivova@infinityworks.com>
2023-01-26 15:02:48 +0000

    setting up a new project

---
+++ Instagram/Instagram/ViewViewModel/Profile/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Instagram
+//
+//  Created by katka.slivova on 26/01/2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("ProfileView")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
---
+++ Instagram/Instagram/ViewViewModel/Search/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  Instagram
+//
+//  Created by katka.slivova on 26/01/2023.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("SearchView")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
