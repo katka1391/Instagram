ee80827cbc4aca8e64c7a7aec5d1e8b801126e5e
Katka Slivova <katka1391@gmail.com>
2023-01-26 15:02:48 +0000

    setting up a new project

---
+++ Instagram/Instagram/ViewViewModel/Notifications/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  Instagram
+//
+//  Created by katka.slivova on 26/01/2023.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Notification View")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
