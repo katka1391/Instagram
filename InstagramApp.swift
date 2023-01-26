08c3a1871662afecdc78bc3ce4e82277e53cf9c1
Katarina Slivova <katka.slivova@infinityworks.com>
2023-01-26 14:32:19 +0000

    setting up a new project

---
+++ InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by katka.slivova on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
